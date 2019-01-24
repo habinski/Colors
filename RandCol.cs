568ce58dac796e96dd9bb579d9b624a2329d1d5d
Dabidibu <matthewschmtz@gmail.com>
2019-01-24 22:09:42 +0100

    Update RandCol.cs

--- RandCol.cs
+++ RandCol.cs
@@ -4,8 +4,6 @@ using System.Collections;
 public class RandCol : MonoBehaviour
 
 
-
-
 {
     public bool main = false, right = false;
 
@@ -17,11 +15,15 @@ public class RandCol : MonoBehaviour
             aColor = new Vector4(Random.Range(0.1f, 1f), Random.Range(0.1f, 1f), Random.Range(0.1f, 1f), 1);
     }
 
+
     void Start()
     {
         if (main || right)
             GetComponent<Renderer>().material.color = aColor;
         else
             GetComponent<Renderer>().material.color = new Vector4(aColor.r + Random.Range(0.1f, 0.3f), aColor.g + Random.Range(0.1f, 0.3f), aColor.b + Random.Range(0.1f, 0.3f), aColor.a);
+    
     }
 }
+
+
